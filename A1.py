39dc51b3d111d0df1da4eec6ab022e0ef12bce6f
sanju6969420 <131761116+sanju6969420@users.noreply.github.com>
2024-03-22 09:48:35 +0530

    hi hello

---
+++ A1.py
@@ -0,0 +1,35 @@
+class queue:
+    def __init__(self):
+        self.queue=[]
+    def insert(self):
+        data=int(input("enter the number"))
+        self.queue.append(data)
+    def delete(self):
+        if len(self.queue)<1:
+            return None
+        else:
+            print("removed element is",self.queue.pop(0))
+    def display(self):
+        if (len(self.queue)==0):
+            print("queue empty")
+        else:
+            print(self.queue)
+
+q=queue()
+while True:
+    print('queue operation\n'
+          '1.append\n'
+          '2.delete\n'
+          '3.display\n'
+          '4.exit')
+    c=int(input("enter your choice"))
+    if c==1:
+        q.insert()
+    elif c==2:
+        q.delete()
+    elif c==3:
+        q.display()
+    elif c==4:
+        break
+    else:
+        print("invalid input")
